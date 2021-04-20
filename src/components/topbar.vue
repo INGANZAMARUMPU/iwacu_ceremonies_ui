1a1ef490fda3e98cb410422b132aca7d37c4d264
konstrictor <jonkur96@gmail.com>
2021-04-20 22:19:38 +0200

    orginized everything and codded navbar

---
+++ src/components/topbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="nav">
+    	<div class="logo">
+    		<router-link to="/">AGATUTU</router-link>
+    	</div>	
+    	<div class="left">
+    		<router-link to="/create" class="menu">Ajouter</router-link>
+    		<router-link to="/login" class="menu">S'abonner</router-link>
+    		<button>Se Connecter</button>
+    	</div>
+    </div>
+</template>
+<script>
+	
+</script>
+<style>
+#nav{
+	background-color: var(--primary);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 50px;
+}
+#nav *{
+	color: var(--white);
+}
+.logo{
+	margin-left: 8px;
+}
+.menu{
+	padding: 8px;
+	margin: 8px;
+}
+.menu:hover{
+	background-color: var(--primary-hover);
+}
+</style>
