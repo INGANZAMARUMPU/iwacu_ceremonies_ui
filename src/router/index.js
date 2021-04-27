6f77cc950a4072831ccca3af4c32789eb7821200
konstrictor <jonkur96@gmail.com>
2021-04-27 23:41:58 +0200

    login page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import List from '../views/List'
 import Create from '../views/Create'
 import Details from '../views/Details'
 import Register from '../views/Register'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
@@ -12,6 +13,7 @@ const routes = [
   { path: '/',name: 'home', component: Home},
   { path: '/list',name: 'list', component: List},
   { path: '/register',name: 'register', component: Register},
+  { path: '/login',name: 'login', component: Login},
   { path: '/list/:prix/:localite',name: 'list', component: List},
   { path: '/create',name: 'create', component: Create},
   { path: '/details/:id',name: 'details', component: Details},
