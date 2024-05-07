3df4f44a5b7e062200ff4167ac45718be5d435fc
mudjaycker <tombutoyi@gmail.com>
2024-05-07 05:35:39 +0200

    Delete Login and Register Views from routers

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import Home from '../views/Home'
 import List from '../views/List'
 import Create from '../views/Create'
 import Details from '../views/Details'
-import Register from '../views/Register'
-import Login from '../views/Login'
 
 
 const routes = [
@@ -12,8 +10,6 @@ const routes = [
   { path: '/', name:'home', component:List},
   { path: '/mine', name:'mine', component:List},
   // { path: '/list', name:'list', component:List},
-  { path: '/register', name:'register', component:Register},
-  { path: '/login', name:'login', component:Login},
   { path: '/list/:prix/:localite', name:'search', component:List},
   { path: '/create', name:'create', component:Create},
   { path: '/details/:salle_name', name:'details', component:Details},
