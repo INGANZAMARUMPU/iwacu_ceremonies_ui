5c4300a0a6cb1875f1b64000a6c3812c20fd57ac
mudjaycker <tombutoyi@gmail.com>
2024-05-07 05:36:13 +0200

    Auth Popup

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -1,33 +1,30 @@
 <template>
-  <!-- <button class="open-button" @click="openForm">Open Form</button> -->
-  <section ref="popup" id="popup" @click="closeForm">
-    <div class="form-popup" ref="form">
-      <form action="/action_page.php" class="form-container">
-        <h1>Login</h1>
-
-        <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" required />
-
-        <label for="psw"><b>Password</b></label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-        />
-
-        <button type="submit" class="btn">Login</button>
-      </form>
-    </div>
+  <section ref="popup" id="popup">
+    <Login @click="" v-if="action == 0" />
+    <Register @click="" v-else />
   </section>
 </template>
 
 <script>
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
 export default {
+  props: ["action"],
+  components: { Login, Register },
   methods: {
     closeForm() {
-      this.$store.state.auth_popup = false
+      this.$store.state.auth_popup = false;
     },
+  },
+  mounted() {
+    document.body.addEventListener(
+      "mouseup",
+      (evt) => {
+        if (evt.target.id == "popup") this.$store.state.auth_popup = false;
+        console.log(evt);
+      },
+      true
+    );
   },
 };
 </script>
@@ -52,66 +49,5 @@ body {
   z-index: 100;
 }
 
-/* Button used to open the contact form - fixed at the bottom of the page */
-.open-button {
-  background-color: #555;
-  color: white;
-  padding: 16px 20px;
-  border: none;
-  cursor: pointer;
-  opacity: 0.8;
-  position: fixed;
-  bottom: 23px;
-  right: 28px;
-  width: 280px;
-}
-
 /* The popup form - hidden by default */
-
-/* Add styles to the form container */
-.form-container {
-  max-width: 300px;
-  padding: 10px;
-  background-color: white;
-}
-
-/* Full-width input fields */
-.form-container input[type="text"],
-.form-container input[type="password"] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  border: none;
-  background: #f1f1f1;
-}
-
-/* When the inputs get focus, do something */
-.form-container input[type="text"]:focus,
-.form-container input[type="password"]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Set a style for the submit/login button */
-.form-container .btn {
-  background-color: #04aa6d;
-  color: white;
-  padding: 16px 20px;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  margin-bottom: 10px;
-  opacity: 0.8;
-}
-
-/* Add a red background color to the cancel button */
-.form-container .cancel {
-  background-color: red;
-}
-
-/* Add some hover effects to buttons */
-.form-container .btn:hover,
-.open-button:hover {
-  opacity: 1;
-}
 </style>
